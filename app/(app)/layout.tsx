26cd0d5cb1b000e05466363710f7d6bf97bdd9d6
Farmaan16 <farmanmuzammil17gmail.com>
2024-09-11 14:03:59 +0530

    Fixed root layout error

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,28 +1,25 @@
 "use client";
 
-import Navbar from "@/components/ui/Navbar";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { store } from "../store/store";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 
+// Define the props interface
 interface RootLayoutProps {
   children: React.ReactNode;
-  session: any;
 }
 
-
-export default function RootLayout({ children, session }: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       <Provider store={store}>
         <DndProvider backend={HTML5Backend}>
           <div className="flex flex-col min-h-screen">
-            {/* <Navbar /> */}
+            {/* Add Navbar component here if needed */}
             {children}
             <Toaster />
           </div>
